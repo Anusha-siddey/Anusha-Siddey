b9601f08c1f51077559d2baf1519a47838cd27ff
Anusha-siddey <63209047+Anusha-siddey@users.noreply.github.com>
2020-04-17 00:13:43 +0530

    Add files via upload

---
+++ Case6/Case6a.cc
@@ -0,0 +1,55 @@
+/*program to explain the operator overloading
+filename:case6.cc
+*/
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+class players
+{
+	char name[20];
+	public:
+	players() //constructor
+	{
+		cout<<"constructor is called::"<<endl;
+	}
+	//fucntion to read the names	
+	void readname()
+	{
+		cout<<"Enter the name:"<<endl;
+		cin>>name;
+	}
+//function to display the names
+	void displayname()
+	{
+		cout<<"Player Fullname::"<<name<<endl;
+	}
+//function to cancatenation of two string using operator overloading
+	players operator+(players x)
+	{
+		players p;
+		strcpy(p.name,name);
+		strcat(p.name,x.name);
+		return p;
+	}
+
+};
+	
+int main(int argc,char* argv[])
+{
+	if(argc==2)
+	{
+		cout<<"Usage :: ./a.out"<<endl;
+		cout<<"Explains the operator overloading"<<endl;
+	}
+	else
+	{
+	players obj1,obj2,obj3;
+	obj1.readname();
+	obj2.readname();
+	obj3=obj1+obj2;
+
+	obj3.displayname();
+	}
+return 0;
+}
---
+++ Case6/Case6b.cc
@@ -0,0 +1,50 @@
+/*program that explain the int* char[] as private members
+filename:case6a.cc
+*/
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Player{
+	int *iAge;
+	char cName[20]
+	;
+	public:
+	Player()
+	{
+		cout<<"constructor called"<<endl;
+	} 
+	//parameterised constructor of player class 
+		Player(int *iage,char *cplayername){
+			cout<<"Called Constructor";
+			iAge=iage;
+			strcpy(cName,cplayername);
+		}
+		void Read(int *iage,char cplayername[]){
+			iAge=iage;
+			strcpy(cName,cplayername);
+		}
+	//display function
+		void display(){
+			cout<<"Age of player::"<<*iAge<<endl;
+			cout<<"Name of the player::"<<cName<<endl;
+		}
+};
+   int main(int argc,char *argv[]){
+   if(argc == 2)
+   {
+        cout<<"usage:./a.out"<<endl;
+       }
+       else
+       {
+       Player obj;
+       int val=30;
+	       //passing values through object
+    char cname[20]="Sachin";
+	obj.Read(&val,cname);
+	obj.display();
+	
+       Player obj2(&val,"Indian Player");
+	obj2.display();
+	return 0;
+}
+}
