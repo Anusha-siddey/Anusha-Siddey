26b36c51d4a58260d4a65f5452bc80ca8e3f89a2
Anusha-siddey <63209047+Anusha-siddey@users.noreply.github.com>
2020-04-17 15:43:21 +0530

    Update case3b.cc

--- Case3/case3b.cc
+++ Case3/case3b.cc
@@ -3,22 +3,30 @@
 #include <iostream>
 using namespace std;
 
-// main function 
-int main()
+// main function
+int main(int argc,char* argv[])
 {
+        if(argc==2)
+        {
+                cout<<"Usage: ./a.out"<<endl;
+                cout<<"Explains the type modifiers"<<endl;
+        }
+else
+{
+
   short int value1;
   long int value2;
   unsigned short value3;
   signed short value4;
-  
+
   cout << "enter value: ";
   cin>>value2;
-  value1=value3=value4=value2;    //value of value1, value2, value3 and value4 is assigned same 
- 
+  value1=value3=value4=value2;    //value of value1, value2, value3 and value4 is assigned same
+
   cout << " short int value1 = " << value1 << endl;        //outputs of the values
   cout << " long int value2 = " << value2 << endl;
   cout << " unsigned short value3 = " << value3 << endl;
   cout << " signed short value4 = " << value4 << endl;
-  
+ }
   return 0;
 }
