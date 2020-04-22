f3c3a70e208ce2a2effad46cd6eccf9b14f34a84
Anusha-siddey <63209047+Anusha-siddey@users.noreply.github.com>
2020-04-22 10:18:59 +0530

    Add files via upload

--- Case6/Case6.cc
+++ Case6/Case6.cc
@@ -1,84 +1,78 @@
-/*porgram using int* and char[20] that explains the overloading/constructor and destructor
-filename:case6.cc
-Date:11/04/2020
-*/
+//Program using the private members of int* and char[] 
 #include<iostream>
-using namespace std ;
-
-class Sampletest
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+class Players
 {
-	// private members
-	int iValue ;
-	int *iPtr;
-	char name[20] ;
-	
-		// public members
-	public:
-		// default constructor
-		Sampletest()
-		{			
-			cout<<"Default constructor is called"<< endl ;
-			
-			cout<<"Enter the ivalue:"<<endl;
-			cin>>iValue;
-		}
-		// destructor
-		~Sampletest()
-		{
-			cout<<"Destructor called" << endl ;
-		}
-
-		//operator overloading
-	void operator--(int)
+//declaration of variables in private
+	char name[20];
+	int *iage;
+public:
+//default constructor
+	Players()
 	{
-		iValue--;
+		cout<<"Default constructor"<<endl;
 	}
-		//  function to read inputs
-		int getinput()
-		{
-			iPtr= &iValue ;
-			//cout<< "enter a integer value " << endl  ; 
-			//cin >> iValue ;
-			
-			cout<<"enter a name"<< endl ;
-			cin >> name ;
-		}
+//parametrised constructor
+	Players(char *playername)
+	{
+		cout<<"Parametrised constructor"<<endl;
+		strcpy(name,playername);//coping string
+	}
+//Show function
+// Return type:void
+	void Show()
+	{
+		cout<<"Player's Age is"<<*iage<<endl;
+	}
+//Display function
+//Return type:void
+	void Display()
+	{
+		cout<<"Player's Full Name is ::"<<name<<endl;
+	}
+//overloadding oprator'+'
+	Players operator+(Players s)
+	{
+		Players temp=name;
+		strcat(temp.name,s.name);	//Concatenation of  two strings
+		return temp;
+	}
 
-		//  function to display outputs
-		
-		int display()
-		{
-			cout<<"------- Display------"<<endl;
-			cout<< "character is :" << name <<endl ;
-			cout<<"---------------"<<endl;
-			cout<< "Adress of integer value iNum:"<< iPtr << endl ;
-			cout<<"---------------"<<endl;
-			cout<< "After decrement is:"<< iValue << endl ;
-			cout<<"---------------"<<endl;
-			cout<< "Value in pointer variable *iPtr is:"<< *iPtr << endl ;
-		}
-		
-} ;
+//putvalue function
+//return type void
+	void putvalue(int *ivalue)
+	{
+	iage=ivalue;
+	}
 
-int main(int argc, char* argv[])
+	~Players()
+	{
+	cout<<"Destructor"<<endl;
+	}
+};
+int main(int argc,char* argv[])
 {
-	// condition for arguments to dispaly usage of the file
-    if (argc == 2)
-    {
-        cout << "usage of the program:  ./a.out " << endl ; 
-		cout << "using int* , char[] as private member variables " << endl ;
-		
-    
-    }
-    
-    else
-    {
-    // object creation	
-	Sampletest obj ;	
-	// object for opperator overloading
-	obj-- ;
-	//functions invoking
-	obj.getinput() ;
-	obj.display() ;
+	if(argc==2)
+	{
+	cout<<"Usage: ./a.out"<<endl;
+	cout<<"Member function and operator overloading"<<endl;
+	}
+	else
+	{
+	int iAg;
+	cout<<"Enter Age:"<<endl;
+	cin>>iAg;
+	//creating objects for the class
+	Players str1("Sachin");
+	Players str2=("Tendulkar");
+	Players str3;
+	Players s;
+	str3=str1+str2; //adding of two strings
+	s.putvalue(&iAg);
+	s.Show();
+	str3.Display();
 	}
 }
+
